29c4cc1b9dc799279aef0a921d848811c8f3f8c8
desniland <albertdesmond26@gmail.com>
2024-05-15 14:02:04 +0300

    Fix a bug1

--- 0x01-challenge/user.py
+++ 0x01-challenge/user.py
@@ -3,23 +3,29 @@
 User class
 """
 
-class User:
+class User():
+    """ Documentation """
+
     def __init__(self):
+        """ Documentation """
         self.__email = None
 
     @property
     def email(self):
-        """Getter for email"""
+        """ Documentation """
         return self.__email
 
     @email.setter
     def email(self, value):
-        """Setter for email"""
-        if not isinstance(value, str):
+        """ Documentation """
+        if type(value) is not str:
             raise TypeError("email must be a string")
         self.__email = value
 
+  
+    
 if __name__ == "__main__":
+
     u = User()
     u.email = "john@snow.com"
     print(u.email)
